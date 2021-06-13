d30993af667c98168e347e389bc90ec8d952abf6
Henrique <hgqn.snf20@uea.edu.br>
2021-06-13 15:48:35 -0300

    #update: adding subjects in class teacher

--- Teacher.py
+++ Teacher.py
@@ -1,3 +1,7 @@
 class Teacher:
     def __init__(self, name):
         self.name = name
+        self.subjects = []
+
+    def add_subjects(self, new_subject):
+        self.subjects.append(new_subject)
