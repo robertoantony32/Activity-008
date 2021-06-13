fc32c6c4358430399e089518465d7c53cb5c4b66
Roberto Antônio <74279517+robertoantony32@users.noreply.github.com>
2021-06-13 16:29:01 -0300

    #feature: adding a new method a university class.,

--- university.py
+++ university.py
@@ -1,3 +1,11 @@
+from Departament import Departament
+
 class University:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
+        self.__departaments = [Departament]
+
+    
+    def add_departament(self, new_departament):
+        self.__departaments.append(new_departament)
+        
