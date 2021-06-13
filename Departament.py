419d970043da69301b9d7b2c008c25cf0323188d
Angelo Marcio Souza Lopes Filho <60992454+angelo-filho@users.noreply.github.com>
2021-06-13 16:25:36 -0400

    #Update: Add new method to Departament

--- Departament.py
+++ Departament.py
@@ -3,8 +3,14 @@ class Departament:
         self.__name = name
         self.__teachers = [first_teacher]
 
+    def get_name(self):
+        return self.__name
+
     def add_teacher(self, new_teacher):
         self.__teachers.append(new_teacher)
+
+    def remove_teacher(self, teacher):
+        self.__teachers.remove(teacher)
 
     def get_teachers(self):
         return self.__teachers
